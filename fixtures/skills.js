c27b2ade8d77a5559868997f9d81dcb7c144c587
aegypius <innercircle@aegypius.com>
2015-07-23 13:56:04 +0200

    Add Features for skills and feats

---
+++ fixtures/skills.js
@@ -0,0 +1,129 @@
+export default [{
+    name: 'Appraise',
+    keyAbility: 'Int'
+}, {
+    name: 'Balance',
+    keyAbility: 'Dex',
+    armourCheckPenalty: true
+}, {
+    name: 'Bluff',
+    keyAbility: 'Cha'
+}, {
+    name: 'Climb',
+    keyAbility: 'Str',
+    armourCheckPenalty: true
+}, {
+    name: 'Conventration',
+    keyAbility: 'Con'
+}, {
+    name: 'Craft (alchemy)',
+    keyAbility: 'Int'
+}, {
+    name: 'Craft (herbalism)',
+    keyAbility: 'Int'
+}, {
+    name: 'Craft (any mundane)',
+    keyAbility: 'Int'
+}, {
+    name: 'Decipher Script',
+    keyAbility: 'Int'
+}, {
+    name: 'Diplomacy',
+    keyAbility: 'Cha'
+}, {
+    name: 'Disable Device',
+    keyAbility: 'Int'
+}, {
+    name: 'Disguise',
+    keyAbility: 'Cha'
+}, {
+    name: 'Escape Artist',
+    keyAbility: 'Dex',
+    armourCheckPenalty: true
+}, {
+    name: 'Forgery',
+    keyAbility: 'Int'
+}, {
+    name: 'Gather Information',
+    keyAbility: 'Cha'
+}, {
+    name: 'Handle Animal',
+    keyAbility: 'Cha'
+}, {
+    name: 'Heal',
+    keyAbility: 'Wis'
+}, {
+    name: 'Hide',
+    keyAbility: 'Dex',
+    armourCheckPenalty: true
+}, {
+    name: 'Intimidate',
+    keyAbility: 'Cha'
+}, {
+    name: 'Jump',
+    keyAbility: 'Str',
+    armourCheckPenalty: true
+}, {
+    name: 'Knowledge (arcana)',
+    keyAbility: 'Int'
+}, {
+    name: 'Knowledge (geography)',
+    keyAbility: 'Int'
+}, {
+    name: 'Knowledge (history)',
+    keyAbility: 'Int'
+}, {
+    name: 'Knowledge (local)',
+    keyAbility: 'Int'
+}, {
+    name: 'Knowledge (nobility)',
+    keyAbility: 'Int'
+}, {
+    name: 'Knowledge (religion)',
+    keyAbility: 'Int'
+}, {
+    name: 'Listen',
+    keyAbility: 'Wis'
+}, {
+    name: 'Move Silently',
+    keyAbility: 'Dex',
+    armourCheckPenalty: true
+}, {
+    name: 'Open Lock',
+    keyAbility: 'Dex'
+}, {
+    name: 'Perform',
+    keyAbility: 'Cha'
+}, {
+    name: 'Profession',
+    keyAbility: 'Wis'
+}, {
+    name: 'Ride',
+    keyAbility: 'Dex'
+}, {
+    name: 'Search',
+    keyAbility: 'Int'
+}, {
+    name: 'Sense Motive',
+    keyAbility: 'Wis'
+}, {
+    name: 'Sleight of Hand',
+    keyAbility: 'Dex',
+    armourCheckPenalty: true
+}, {
+    name: 'Spot',
+    keyAbility: 'Wis'
+}, {
+    name: 'Survival',
+    keyAbility: 'Wis'
+}, {
+    name: 'Swim',
+    keyAbility: 'Str'
+}, {
+    name: 'Tumble',
+    keyAbility: 'Dex',
+    armourCheckPenalty: true
+}, {
+    name: 'Use Rope',
+    keyAbility: 'Dex'
+}];
